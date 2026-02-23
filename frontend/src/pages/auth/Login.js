693ea65f7051fdd0d7282e3bef7e83cdb6588c9a
Himanshu Raj <76990043+HimanshuRaj11@users.noreply.github.com>
2026-02-23 13:13:57 -0400

    update login url

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
         setError('');
         setLoading(true);
         try {
-            const { data } = await api.post('/auth/login', form);
+            const { data } = await api.post('https://satyam-motor-parts.vercel.app/auth/login', form);
             login(data);
             if (data.role === 'admin') navigate('/admin/dashboard');
             else if (data.role === 'owner') navigate('/owner/dashboard');
